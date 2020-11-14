7d41a695a6093175975c68d3e6368eda77353fd2
alsu <surmeeva.alsu@gmail.com>
2020-11-14 09:17:48 +0300

    Hotfixes
    * Time now measured in nanoseconds
    ! Fix file reading/writing

--- main.cpp
+++ main.cpp
@@ -13,96 +13,94 @@ using T = int;
 using Tree = std::shared_ptr<AbstractTree<T>>;
 
 
-class TestingSystem
-{
-    private:
+class TestingSystem {
+private:
     int M = 10;
-    public:
+public:
     Tree my_tree;
+
     //не понимаю, когда объявлять, когда опредять конструктор, надо ли туда что-то писать и что;
-    explicit TestingSystem(Tree my_tree)
-    {
-        this -> my_tree = my_tree;
+    explicit TestingSystem(Tree my_tree) {
+        this->my_tree = my_tree;
     }
+
     ~TestingSystem()
     = default;
 
-    static double get_time()
-    {
+    static double get_time() {
         return std::chrono::duration_cast<std::chrono::microseconds>
-                       (std::chrono::steady_clock::now().time_since_epoch()).count()/1e6;
+                (std::chrono::steady_clock::now().time_since_epoch()).count(); // Будем использовать наносекунды, окда?
     }
 
-using time_t = double;
-
-    void checking_all(std::vector<int> & number_tests, std::string & final_file)
-    {
-        for(int k = 0; k < M; k++)
-        {
-            std::ofstream ofs_final(final_file); //без &?
-            time_t start = get_time();
-            for(int i = 0; i < number_tests.size(); i++)
-            {
-                my_tree -> add(number_tests[i]);
+    void checking_all(std::vector<int> &number_tests, std::string &final_file) {
+        std::vector<double> add, check, remove;
+        for (int k = 0; k < M; k++) {
+            double start = get_time();
+            for (int number_test : number_tests) {
+                my_tree->add(number_test);
             }
-            time_t end = get_time();
-            std::cout << (end - start) / number_tests.size() << " "; //time for n = number_tests.size() elements
+            double end = get_time();
+            add.push_back((end - start) / number_tests.size());
+
             start = get_time();
-            for(int i = 0; i < number_tests.size(); i++)
-            {
-                my_tree -> check(number_tests[i]);
+            for (int number_test : number_tests) {
+                my_tree->check(number_test);
             }
             end = get_time();
-            std::cout << (end - start) / number_tests.size() << " ";
+            check.push_back((end - start) / number_tests.size());
+
             start = get_time();
-            for (int i = 0; i < number_tests.size(); i++)
-            {
-                my_tree -> remove(number_tests[i]);
+            for (int number_test : number_tests) {
+                my_tree->remove(number_test);
             }
             end = get_time();
-            std::cout << (end - start) / number_tests.size() << " ";
-            std::cout << "the " << k << "-th" << " running from" << M;
-
+            remove.push_back((end - start) / number_tests.size());
         }
+        std::ofstream ofs_final(final_file); //без &? Да. без &.
+        for (auto a : add)
+            ofs_final << a << " ";
+        ofs_final << std::endl;
+        for (auto a : check)
+            ofs_final << a << " ";
+        ofs_final << std::endl;
+        for (auto a : remove)
+            ofs_final << a << " ";
+        ofs_final << std::endl;
     }
 
 };
 
 
 int main() {
-   // Tree tree(new AVLTree<int>());
+    // Tree tree(new AVLTree<int>());
     std::shared_ptr<AbstractTree<T>> tree(new BlackRedTree<T>());
     std::string tree_file, number_file, final_file;
-    std::cout << "Please, enter names of 3 files:1)for creation of the tree; 2)with numbers for tests; 3)for final answers";
+    std::cout
+            << "tree? tests? answers?" << std::endl;
     std::cin >> tree_file >> number_file >> final_file;
 
     int size_of_tree = 0;
     std::ifstream ifs_tree(tree_file);
-    if (ifs_tree.is_open())
-    {
+    if (ifs_tree.is_open()) {
         int a = 0;
-        while(std::cin >> a)
-        {
+        while (ifs_tree >> a) {
             size_of_tree += 1;
-            tree -> add(a);
+            tree->add(a);
         }
     }
     ifs_tree.close();
     std::ofstream ofs_final(final_file);
-    std::cout  << "size of tree = " << size_of_tree << std::endl;
+    std::cout << "size of tree = " << size_of_tree << std::endl;
     ofs_final.close();
     std::vector<int> numbers_for_test;
     std::ifstream ifs_test(number_file);
-    if (ifs_test.is_open())
-    {
+    if (ifs_test.is_open()) {
         int a = 0;
-        while(std::cin >> a)
-        {
+        while (ifs_test >> a) {
             numbers_for_test.push_back(a);
         }
     }
     ifs_test.close();
-
 
     TestingSystem(tree).checking_all(numbers_for_test, final_file);
 }
